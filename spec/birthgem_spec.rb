8d35e6133dbe6d6359b7944bd547e90124dfc08f
kaisumi <kaisumi@langue-lab.com>
2022-01-18 11:40:24 +0900

    added some tests.

--- spec/birthgem_spec.rb
+++ spec/birthgem_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe Birthgem do
     expect(Birthgem::VERSION).not_to be nil
   end
 
-  it 'outputs a birthstone' do
+  it 'outputs a birthstone with string' do
     expect(Birthgem.stone('2022-1-18')).to eq %w[Garnet]
+  end
+
+  it 'outputs a birthstone with number' do
+    expect(Birthgem.stone(10)).to eq %w[Opal Tourmaline]
+  end
+
+  it 'outputs a birthstone with date' do
+    require 'date'
+    birthday = Date.new(1993, 2, 24)
+    expect(Birthgem.stone(birthday)).to eq %w[Amethyst]
   end
 end

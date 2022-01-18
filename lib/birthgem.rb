cfcf25af794769d9ebe6e804e94e0b298974acec
kaisumi <kaisumi@langue-lab.com>
2022-01-18 10:04:30 +0900

    method 'stone' added.

--- lib/birthgem.rb
+++ lib/birthgem.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 require_relative 'birthgem/version'
+require 'date'
 
 module Birthgem
   class Error < StandardError; end
-  # Your code goes here...
+  STONE_BY_MONTH = {
+    1 => %w[Garnet],
+    2 => %w[Amethyst],
+    3 => %w[Aquamarine],
+    4 => %w[Diamond],
+    5 => %w[Emerald],
+    6 => %w[Pearl Alexandrite],
+    7 => %w[Ruby],
+    8 => %w[Peridot],
+    9 => %w[Sapphire],
+    10 => %w[Opal Tourmaline],
+    11 => %w[Citrine],
+    12 => %w[Blue Topaz Tanzanite Blue Zircon]
+  }.freeze
+  def stone(date)
+    parsed_date = Date.parse(date)
+    STONE_BY_MONTH(parsed_date.month)
+  end
 end
